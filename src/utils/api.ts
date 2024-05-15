ee8bc77b157c881a2a642dc9d3979b1ccaa71bc0
Alex Kharlamov <aakharlam@gmail.com>
2024-05-16 02:50:54 +0400

    feat: add api file for requests

---
+++ src/utils/api.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://127.0.0.1:3000';
+
+// Cars
+export const getCars = async () => {
+  const res = await axios.get(`${BASE_URL}/garage`);
+
+  return res;
+};
+
+export const getCar = async (carId: string) => {
+  const res = await axios.get(`${BASE_URL}/garage/${carId}`);
+
+  return res.data;
+};
+
+export const createCar = async (color: string, name: string) => {
+  const res = await axios.post(`${BASE_URL}/garage`, { name, color });
+
+  return res.data;
+};
+
+export const deleteCar = async (carId: string) => {
+  const res = await axios.delete(`${BASE_URL}/garage/${carId}`);
+
+  return res.data;
+};
+
+export const updateCar = async (carId: string, color: string, name: string) => {
+  const res = await axios.put(`${BASE_URL}/garage/${carId}`, { name, color });
+
+  return res.data;
+};
+
+// Drive
+export const requestRace = async (carId: string, status: string) => {
+  const res = await axios.patch(`${BASE_URL}/engine`, null, {
+    params: { id: carId, status },
+  });
+
+  return res.data;
+};
