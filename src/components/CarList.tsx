60a4b7bbab7460931b23d5ef430d0d74df0207e2
Alex Kharlamov <aakharlam@gmail.com>
2024-05-16 04:44:03 +0400

    feat: replace mock car items with real

--- src/components/CarList.tsx
+++ src/components/CarList.tsx
@@ -1,6 +1,7 @@
 import { Cars } from '../types/types.ts';
 import usePagination from '../hooks/usePagination.tsx';
 import PaginationButtons from './ui/PaginationButtons.tsx';
+import CarItem from './CarItem/CarItem.tsx';
 
 type Props = {
   cars: Cars;
@@ -11,18 +12,18 @@ export default function CarList({ cars }: Props) {
     usePagination(cars);
 
   return (
-    <div>
-      <PaginationButtons
-        currentPage={currentPage}
-        pageCount={pageCount}
-        setNextPage={setNextPage}
-        setPrevPage={setPrevPage}
-      />
+    <div className="p-2">
+      <div className="ml-auto flex justify-end">
+        <PaginationButtons
+          currentPage={currentPage}
+          pageCount={pageCount}
+          setNextPage={setNextPage}
+          setPrevPage={setPrevPage}
+        />
+      </div>
 
       {paginatedData.map((car) => (
-        <p className="mb-2" key={car.id}>
-          {car.name}
-        </p>
+        <CarItem key={car.id} car={car} />
       ))}
     </div>
   );
