465d17ff54e6d6ed5209847a3c8f1756c9fd3257
Alex Kharlamov <aakharlam@gmail.com>
2024-05-16 14:17:00 +0400

    fix: adaptive text

--- src/components/CarItem/CarInfo.tsx
+++ src/components/CarItem/CarInfo.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function CarInfo({ car }: Props) {
   return (
-    <p className="absolute top-1/2 left-5 text-3xl font-semibold -translate-y-1/2 opacity-50 -z-50">
+    <p className="absolute top-1/2 left-5 text-1xl lg:text-3xl font-semibold -translate-y-1/2 opacity-50 -z-50">
       {car.name}
     </p>
   );
