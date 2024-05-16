ae9dfd2cc37e832e3941e37e57881c47428290be
Alex Kharlamov <aakharlam@gmail.com>
2024-05-16 06:00:38 +0400

    fix: delete id from car info

--- src/components/CarItem/CarInfo.tsx
+++ src/components/CarItem/CarInfo.tsx
@@ -7,7 +7,6 @@ type Props = {
 export default function CarInfo({ car }: Props) {
   return (
     <p className="absolute top-1/2 left-5 text-3xl font-semibold -translate-y-1/2 opacity-50 -z-50">
-      {car.id}
       {car.name}
     </p>
   );
