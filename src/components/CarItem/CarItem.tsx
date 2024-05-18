204b11cd8383973ac2b101f4e2374c88212d3e47
Alex Kharlamov <aakharlam@gmail.com>
2024-05-18 16:00:03 +0400

    refactor: extract animation logic to hook

--- src/components/CarItem/CarItem.tsx
+++ src/components/CarItem/CarItem.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { AnimationState, Car } from '../../types/types.ts';
-import config from '../../data/config.ts';
+import { Car } from '../../types/types.ts';
 import Track from './Track.tsx';
 import StatusMessage from './StatusMessage.tsx';
 import MoveButtons from './MoveButtons.tsx';
 import ManageCarButtons from './ManageCarButtons.tsx';
-
-const { BASE_URL } = config;
+import useAnimation from '../../hooks/useAnimation.tsx';
 
 type Props = {
   car: Car;
@@ -15,72 +11,8 @@ type Props = {
 };
 
 export default function CarItem({ car, onUpdate }: Props) {
-  const [animation, setAnimation] = useState<AnimationState>({
-    isAnimating: false,
-    isStopped: true,
-    isPaused: false,
-    x: 0,
-    broken: false,
-    finished: false,
-    duration: 0,
-  });
-
-  const handleStart = async () => {
-    try {
-      const { data } = await axios.patch(
-        `${BASE_URL}/engine/?id=${car.id}&status=started`,
-      );
-      const dataDuration = data.distance / data.velocity;
-
-      setAnimation((prevState) => ({
-        ...prevState,
-        isAnimating: true,
-        isStopped: false,
-        isPaused: false,
-        finished: false,
-        broken: false,
-        duration: dataDuration / 1000,
-      }));
-
-      await axios.patch(`${BASE_URL}/engine/?id=${car.id}&status=drive`);
-    } catch (err) {
-      setAnimation((prevState) => ({
-        ...prevState,
-        isAnimating: false,
-        isPaused: true,
-        broken: true,
-      }));
-    }
-  };
-
-  const handleStop = () => {
-    setAnimation({
-      isAnimating: false,
-      isStopped: true,
-      isPaused: false,
-      x: 0,
-      broken: false,
-      finished: false,
-      duration: 0,
-    });
-  };
-
-  const handleUpdate = (latest: { x: number }) => {
-    if (animation.isAnimating) {
-      setAnimation((prevState) => ({
-        ...prevState,
-        x: latest.x,
-      }));
-    }
-  };
-
-  const handleFinish = () => {
-    if (!animation.isAnimating) return;
-    setAnimation((prevState) => ({
-      ...prevState,
-      finished: true,
-    }));
-  };
+  const { animation, handleFinish, handleStart, handleStop, handleUpdate } =
+    useAnimation(car);
 
   return (
     <div className="mb-2">
--- src/hooks/useAnimation.tsx
+++ src/hooks/useAnimation.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
-import { AnimationState } from '../types/types.ts';
+import axios from 'axios';
+import config from '../data/config.ts';
+import { AnimationState, Car } from '../types/types.ts';
 
-function useAnimation() {
+const { BASE_URL } = config;
+
+function useAnimation(car: Car) {
   const [animation, setAnimation] = useState<AnimationState>({
     isAnimating: false,
     isStopped: true,
@@ -11,6 +15,65 @@ function useAnimation() {
     finished: false,
     duration: 0,
   });
+
+  const handleStart = async () => {
+    try {
+      const { data } = await axios.patch(
+        `${BASE_URL}/engine/?id=${car.id}&status=started`,
+      );
+      const dataDuration = data.distance / data.velocity;
+
+      setAnimation((prevState) => ({
+        ...prevState,
+        isAnimating: true,
+        isStopped: false,
+        isPaused: false,
+        finished: false,
+        broken: false,
+        duration: dataDuration / 1000,
+      }));
+
+      await axios.patch(`${BASE_URL}/engine/?id=${car.id}&status=drive`);
+    } catch (err) {
+      setAnimation((prevState) => ({
+        ...prevState,
+        isAnimating: false,
+        isPaused: true,
+        broken: true,
+      }));
+    }
+  };
+
+  const handleStop = () => {
+    setAnimation({
+      isAnimating: false,
+      isStopped: true,
+      isPaused: false,
+      x: 0,
+      broken: false,
+      finished: false,
+      duration: 0,
+    });
+  };
+
+  const handleUpdate = (latest: { x: number }) => {
+    if (animation.isAnimating) {
+      setAnimation((prevState) => ({
+        ...prevState,
+        x: latest.x,
+      }));
+    }
+  };
+
+  const handleFinish = () => {
+    if (!animation.isAnimating) return;
+    setAnimation((prevState) => ({
+      ...prevState,
+      finished: true,
+    }));
+  };
+
+  return { handleStart, animation, handleStop, handleUpdate, handleFinish };
 }
 
 export default useAnimation;
