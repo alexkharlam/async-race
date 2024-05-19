24ea70858cc181c5faf6c2fb5de60d0cf6fc16f4
Alex Kharlamov <aakharlam@gmail.com>
2024-05-19 13:41:12 +0400

    feat: api winner functions

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { WinnerData } from '../types/types.ts';
 
 const BASE_URL = 'http://127.0.0.1:3000';
 
@@ -40,4 +41,61 @@ export const requestRace = async (carId: string, status: string) => {
   });
 
   return res.data;
+};
+
+// WINNERS
+export const getWinners = async () => {
+  const res = await axios.get(`${BASE_URL}/winners`);
+
+  return res;
+};
+
+const getWinner = async (carId: number) => {
+  const res = await axios.get(`${BASE_URL}/winners/${carId}`);
+
+  return res;
+};
+
+const createWinner = async (winner: WinnerData) => {
+  const res = await axios.post(`${BASE_URL}/winners`, {
+    id: winner.id,
+    wins: 1,
+    time: winner.duration,
+  });
+
+  return res;
+};
+
+const updateWinner = async (
+  winner: WinnerData,
+  oldBestTime: number,
+  oldWins: number,
+) => {
+  const newTime = winner.duration < oldBestTime ? winner.duration : oldBestTime;
+
+  const res = await axios.put(`${BASE_URL}/winners/${winner.id}`, {
+    wins: oldWins + 1,
+    time: newTime,
+  });
+
+  return res;
+};
+
+export const createUpdateWinner = async (winner: WinnerData) => {
+  try {
+    const res = await getWinner(winner.id);
+
+    const { time: oldBestTime, wins: oldWins } = res.data;
+
+    await updateWinner(winner, oldBestTime, oldWins);
+  } catch (err) {
+    if (!axios.isAxiosError(err)) {
+      console.error(err);
+      return;
+    }
+
+    if (err.response?.status === 404) {
+      await createWinner(winner);
+    }
+  }
 };
