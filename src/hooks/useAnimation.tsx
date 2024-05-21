6d5db13924cc802ea1d11c6bf7e30eb55ad61853
Alex Kharlamov <aakharlam@gmail.com>
2024-05-21 21:44:28 +0400

    refactor: add update animation function

--- src/hooks/useAnimation.tsx
+++ src/hooks/useAnimation.tsx
@@ -12,62 +12,52 @@ const initialAnimationState = {
   duration: 0,
 };
 
+const startedAnimationState = {
+  isAnimating: true,
+  isStopped: false,
+  isPaused: false,
+  finished: false,
+  broken: false,
+  x: 0,
+};
+
 function useAnimation(car: Car, onFinish: (winner: NewWinner) => void) {
   const [animation, setAnimation] = useState<AnimationState>(initialAnimationState);
+
+  const updateAnimation = useCallback((updated: Partial<AnimationState>) => {
+    setAnimation((prevState) => ({ ...prevState, ...updated }));
+  }, []);
 
   const handleStart = useCallback(async () => {
     try {
       const { distance, velocity } = await api.startEngine(car.id);
-
       const dataDuration = distance / velocity;
 
-      setAnimation((prevState) => ({
-        ...prevState,
-        isAnimating: true,
-        isStopped: false,
-        isPaused: false,
-        finished: false,
-        broken: false,
-        duration: dataDuration / 1000,
-      }));
-
+      updateAnimation({ ...startedAnimationState, duration: dataDuration / 1000 });
       await api.drive(car.id);
     } catch (err) {
-      setAnimation((prevState) => ({
-        ...prevState,
-        isAnimating: false,
-        isPaused: true,
-        broken: true,
-      }));
+      updateAnimation({ isAnimating: false, isPaused: true, broken: true });
     }
-  }, [car.id]);
+  }, [car.id, updateAnimation]);
 
   const handleStop = useCallback(async () => {
     await api.stopEngine(car.id);
-
     setAnimation(initialAnimationState);
   }, [car.id]);
 
   const handleUpdate = useCallback(
     (latest: { x: number }) => {
-      if (animation.isAnimating) {
-        setAnimation((prevState) => ({
-          ...prevState,
-          x: latest.x,
-        }));
-      }
+      if (!animation.isAnimating) return;
+      updateAnimation({ x: latest.x });
     },
-    [animation.isAnimating],
+    [animation.isAnimating, updateAnimation],
   );
 
   const handleFinish = useCallback(() => {
     if (!animation.isAnimating) return;
-    setAnimation((prevState) => ({
-      ...prevState,
-      finished: true,
-    }));
+    updateAnimation({ finished: true });
     onFinish({ name: car.name, id: car.id, duration: animation.duration });
-  }, [animation.isAnimating, car.id, car.name, onFinish, animation.duration]);
+  }, [animation.isAnimating, car.id, car.name, onFinish, animation.duration, updateAnimation]);
 
   return { handleStart, animation, handleStop, handleUpdate, handleFinish };
 }
