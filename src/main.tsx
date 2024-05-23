a95ede1c226b39e156bf7dfc949b8490f6b549f0
Alex Kharlamov <aakharlam@gmail.com>
2024-05-23 17:39:36 +0400

    fix: router-dom routing method

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Garage from './views/Garage.tsx';
 import Winners from './views/Winners.tsx';
 import routerPaths from './data/routerPaths.ts';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />} errorElement={<p>Hi there, error</p>}>
-      <Route index path={routerPaths.garage} element={<Garage />} />
-      <Route path={routerPaths.winners} element={<Winners />} />
-    </Route>,
-  ),
-);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} errorElement={<p>Hi there, error</p>}>
+          <Route index path={routerPaths.garage} element={<Garage />} />
+          <Route path={routerPaths.winners} element={<Winners />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
